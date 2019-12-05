a1ddf7c028d3aae25f52269b9615feec2624ddbe
Glitch (simple-hortensia) <none>
2019-12-05 12:45:17 +0000

    fixed candidates list

--- constants.js
+++ constants.js
@@ -1,6 +1,6 @@
 module.exports = {
  "POOL_TIMEOUT": 3000,
- "WINNER_PLACEHOLDER":"${USER}",
+ "WINNER_PLACEHOLDER":"${PLACEHOLDER}",
  "HEADERS": { Authorization: "Bearer " + process.env.SLACK_ACCESS_TOKEN },
  "NUM_WIN_ERROR":":exclamation: the number of winners must be a number greater than 0" 
 }
